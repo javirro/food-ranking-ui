ba0118f939c8af18f52b25e133b508c1b05e8507
idcetim <javirofe96@gmail.com>
2023-05-20 10:53:07 +0200

    Add endpoints file

---
+++ src/Api/endpoints.js
@@ -0,0 +1,9 @@
+export const baseURL = "http://localhost:7071/api/";
+
+// const baseURL =  "https://food-ranking-api.azurewebsites.net/"
+
+export const endpoints = {
+  add: baseURL + "add-item",
+  delete: baseURL + "delete-item",
+  get: baseURL + "get-all-items"
+}
