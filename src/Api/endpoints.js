b1d3061e6e43212ba12577316c9756bc33fccd3b
idcetim <javirofe96@gmail.com>
2023-05-27 12:52:07 +0200

    Fix Azure url

--- src/Api/endpoints.js
+++ src/Api/endpoints.js
@@ -1,4 +1,4 @@
-export const baseURL = process.env.REACT_APP_API_KEY ? "https://food-ranking-api.azurewebsites.net/" : "http://localhost:7071/api/";
+export const baseURL = process.env.REACT_APP_API_KEY ? "https://food-ranking-api.azurewebsites.net/api/" : "http://localhost:7071/api/";
 export const endpoints = {
   add: baseURL + "add-item",
   delete: baseURL + "delete-item",
