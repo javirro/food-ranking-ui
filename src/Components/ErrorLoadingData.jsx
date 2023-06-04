e3cf714afdeecd06c1ba6dad49cf314877bbe596
idcetim <javirofe96@gmail.com>
2023-06-04 10:12:00 +0200

    Add errorLoadingData message in ranking

---
+++ src/Components/ErrorLoadingData.jsx
@@ -0,0 +1,9 @@
+import '../Styles/errorLoading.css'
+const ErrorLoadingData = () => {
+  return (<div className="error-loading-container">
+    <span>Error getting data. Try again later.</span>
+  </div>)
+
+}
+
+export default ErrorLoadingData
