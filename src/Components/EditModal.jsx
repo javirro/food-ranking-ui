dc44f37ee5ae7139f2a86f97f7e20135e9441134
idcetim <javirofe96@gmail.com>
2023-10-29 11:53:05 +0100

    Update style for edit modal

--- src/Components/EditModal.jsx
+++ src/Components/EditModal.jsx
@@ -5,9 +5,6 @@ import { validatePosition } from '../ErrorValidation/validator'
 import { InputError } from '../ErrorValidation/CustomizeError'
 import NotAuthorized from './NotAuthorized'
 import '../Styles/editModal.css'
-import '../Styles/modal.css'
-
-
 
 const EditModal = ({ table, row, setIsEditModal, setTrigger }) => {
   const [data, setData] = useState({
@@ -20,7 +17,7 @@ const EditModal = ({ table, row, setIsEditModal, setTrigger }) => {
   const url = endpoints.update
   const token = window.localStorage.getItem("token")
 
-  if(!token) return <NotAuthorized operation={"edit"} setIsModal={setIsEditModal} />
+  if (!token) return <NotAuthorized operation={"edit"} setIsModal={setIsEditModal} />
   const updateRow = () => {
     try {
       validatePosition(data.position)
@@ -54,14 +51,17 @@ const EditModal = ({ table, row, setIsEditModal, setTrigger }) => {
   }
 
   return (
-    <div className="modal">
-      <div className="modal-content" >
-        <header>
-          <button onClick={() => setIsEditModal(false)}>Close</button>
+    <div className="modal-edit">
+      <div className="modal-edit-content" >
+        <header className="header-edit-modal">
+          <button onClick={() => setIsEditModal(false)} className="close-edit-modal-btn">Close</button>
         </header>
         <section className="section-inputs-modal">
+          <label className="edit-modal-labels">Restaurant name</label>
           <input type="text" value={data?.name} onChange={(ev) => setData(s => ({ ...s, name: ev.target.value }))} placeholder="Restaurant Name" className="inputs-edit-modal" maxLength={120} />
+          <label className="edit-modal-labels">Ranking position</label>
           <input type="text" value={data?.position} onChange={(ev) => setData(s => ({ ...s, position: ev.target.value }))} placeholder="Position" className="inputs-edit-modal" />
+          <label className="edit-modal-labels">Ubication</label>
           <input type="text" value={data?.ubication} onChange={(ev) => setData(s => ({ ...s, ubication: ev.target.value }))} placeholder="Ubication" className="inputs-edit-modal" />
           {/* <input type="text" value={`${data?.price} €`} onChange={(ev) => setData(s => ({ ...s, price: ev.target.value }))} placeholder="Price" className="inputs-edit-modal" /> */}
           <button className="button-update" onClick={() => updateRow()}>Update - {row.name}</button>
