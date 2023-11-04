4908536b8222480cb1a9f4bf2b6b80cf377bf3bf
idcetim <javirofe96@gmail.com>
2023-11-04 17:57:05 +0100

    Add faceIn animation to home links

--- src/Routes/Home.jsx
+++ src/Routes/Home.jsx
@@ -4,10 +4,10 @@ const Home = () => {
   return (
     <>
       <main className="home-container">
-        <Link className="links" to="/ranking/cheesecakes"> 🏆 Ranking cheesecakes</Link>
-        <Link className="links" to="/ranking/burgers">🏆 Ranking burgers</Link>
-        <Link className="links" to="/map/cheesecakes"> 🌍 Map cheesecakes</Link>
-        <Link className="links" to="/map/burgers">🌍 Map burgers</Link>
+        <Link className="links animation-link-1" to="/ranking/cheesecakes"> 🏆 Ranking cheesecakes</Link>
+        <Link className="links animation-link-2" to="/ranking/burgers">🏆 Ranking burgers</Link>
+        <Link className="links animation-link-3" to="/map/cheesecakes"> 🌍 Map cheesecakes</Link>
+        <Link className="links animation-link-4" to="/map/burgers">🌍 Map burgers</Link>
         {/* <Link className="links" to="/ranking/restaurants">🏆 Ranking restaurants</Link> */}
       </main>
       <Outlet />
