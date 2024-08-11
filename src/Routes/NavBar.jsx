8b74c47b5ba2c772821958318c2d893bfd169df0
idcetim <javirofe96@gmail.com>
2024-08-11 11:01:30 +0200

    Fix some size

--- src/Routes/NavBar.jsx
+++ src/Routes/NavBar.jsx
@@ -1,7 +1,10 @@
-import '../Styles/navbar.css'
+
 import { NavLink } from "react-router-dom"
 import { useState } from 'react'
 import menuIcon from "../Images/navbar-icon.svg"
+import { ROUTES } from './definitionRoutes'
+
+import '../Styles/navbar.css'
 
 const NavBar = () => {
   const [showMenu, setShowMenu] = useState(false)
@@ -10,9 +13,9 @@ const NavBar = () => {
       <img src={menuIcon} alt="Menu icon" className="nav-icon" onClick={() => setShowMenu(!showMenu)} />
       <div className={`${showMenu ? "nav-items" : "none"}`}>
         <NavLink to="/" className="nav-link"> Home </NavLink>
-        <NavLink to="/cheesecakes" className="nav-link"> Cheesecakes </NavLink>
-        <NavLink to="/burgers" className="nav-link"> Burgers </NavLink>
-        <NavLink to="/restaurants" className="nav-link"> Restaurants </NavLink>
+        <NavLink to={ROUTES.cheesecakes} className="nav-link"> Cheesecakes </NavLink>
+        <NavLink to={ROUTES.burgers} className="nav-link"> Burgers </NavLink>
+        {/* <NavLink to="/restaurants" className="nav-link"> Restaurants </NavLink> */}
       </div>
     </nav>
   )
---
+++ src/Routes/definitionRoutes.js
@@ -0,0 +1,6 @@
+export const ROUTES = Object.freeze({
+  home: "/",
+  cheesecakes: "/cheesecakes",
+  burgers: "/burgers",
+  champions: "/champions-burger",
+})
